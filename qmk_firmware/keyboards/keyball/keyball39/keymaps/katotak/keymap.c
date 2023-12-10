da6ea777ecbc68cf7550c0fc8e654cd73c74213d
katotak <takuyakato.1027@gmail.com>
2023-12-10 11:39:35 +0900

    refactoring

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
@@ -357,8 +357,8 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 ////////////////////////////
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // // Auto enable scroll mode when the highest layer is 3
-    // keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    // Auto enable scroll mode when the highest layer is 2
+    // keyball_set_scroll_mode(get_highest_layer(state) == 2);
 
     uint8_t layer = biton32(state);
     if (layer == click_layer) {
@@ -374,7 +374,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #include "lib/oledkit/oledkit.h"
 
-void mouse_layer_logo(void) {
+void oledkit_render_logo_mouse(void) {
     static const char PROGMEM raw_logo[] = {
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,192,192,192,192,128,  0,  0,  0,  0,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,255,255, 30,112,192,  0,  0,  0,  0,  0,  0,  0,  0,192,112, 28,  7,255,  0,  0,  0,  0,  0,  0,128,128,192, 64, 64, 64, 64,192,128,  0,  0,  0,  0,  0,  0,192,192,  0,  0,  0,  0,  0,  0,  0,192,192,  0,  0,  0,  0,  0,128,192, 64, 64, 64, 64, 64,192,192,  0,  0,  0,  0,  0,128,192,192, 64, 64, 64,192,192,128,  0,  0,  0,  0,  0,  0,  0,207,255,249,248,249,125,127,255,254,255,231,195,199,255,255,126,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -399,7 +399,7 @@ void oledkit_render_info_user(void) {
 void render_user_oled(void) {
   int layer = get_highest_layer(layer_state);
   if (layer == click_layer) {
-    mouse_layer_logo();
+    oledkit_render_logo_mouse();
   } else {
     oledkit_render_info_user();
   }
@@ -433,23 +433,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     S(KC_BSLS), S(KC_RBRC), KC_RBRC   , S(KC_0)   , S(KC_DOT) ,                              KC_LNG2   , S(KC_EQL) , KC_SCLN   , S(KC_QUOT), KC_BSLS   ,
     _______  , _______  , _______  , _______  , LT(3,_______) , _______   ,      _______   , LT(3,_______), _______  , _______  , _______  , _______
   ),
+
   LAYOUT_universal(
     KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                              KC_6      , KC_7      , KC_8      , KC_9      , KC_0      ,
-    KC_HOME   , KC_PGUP   , KC_UP     , KC_PGDN   , KC_INS    ,                        KC_TO_CLICKABLE_INC, KC_MY_BTN1, KC_MY_BTN3, KC_MY_BTN2, KC_MY_SCR ,
-    KC_END    , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_DEL    ,                        KC_TO_CLICKABLE_DEC,ALT_T(KC_BTN4),CTL_T(XXXXXXX),SFT_T(KC_BTN5),XXXXXXX,
+    KC_HOME   , KC_PGUP   , KC_UP     , KC_PGDN   , KC_DEL    ,                              KC_BTN4   , KC_BTN1   , KC_BTN3   , KC_BTN2   , KC_BTN5   ,
+    KC_END    , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_INS    ,                              XXXXXXX  ,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
     _______  , _______  , _______  , _______  , LT(3,_______) , _______   ,      _______   , LT(3,_______), _______  , _______  , _______  , _______
   ),
 
   LAYOUT_universal(
     KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     ,                              KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    ,
-    KC_F11    , KC_F12    , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , 
-    XXXXXXX,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,                            XXXXXXX,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
+    KC_F11    , KC_F12    , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                     KC_TO_CLICKABLE_INC, XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , 
+    XXXXXXX,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,                   KC_TO_CLICKABLE_DEC,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
     _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
   ),
 
   LAYOUT_universal(
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , KC_MY_BTN1, KC_MY_BTN3, KC_MY_BTN2, KC_MY_SCR ,
+    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
+    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
+  ),
+
+  LAYOUT_universal(
+    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
+    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
     _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
   )
