f101a93d9b8de74ed5e56f25086a17ccbda50b89
katotak <takuyakato.1027@gmail.com>
2023-12-10 16:11:15 +0900

    modify

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/config.h
@@ -20,20 +20,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-// #    define RGBLIGHT_EFFECT_BREATHING
-// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #    define RGBLIGHT_EFFECT_SNAKE
-// #    define RGBLIGHT_EFFECT_KNIGHT
-// #    define RGBLIGHT_EFFECT_CHRISTMAS
-// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #    define RGBLIGHT_EFFECT_RGB_TEST
-// #    define RGBLIGHT_EFFECT_ALTERNATING
-// #    define RGBLIGHT_EFFECT_TWINKLE
-#endif
+// 容量削減のため以下をオミット
+// #ifdef RGBLIGHT_ENABLE
+// #define RGBLIGHT_EFFECT_BREATHING
+// #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #define RGBLIGHT_EFFECT_SNAKE
+// #define RGBLIGHT_EFFECT_KNIGHT
+// #define RGBLIGHT_EFFECT_CHRISTMAS
+// #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #define RGBLIGHT_EFFECT_RGB_TEST
+// #define RGBLIGHT_EFFECT_ALTERNATING
+// #define RGBLIGHT_EFFECT_TWINKLE
+// #endif
+
+#define COMBO_COUNT 9
+
+#define KEYBALL_SCROLLSNAP_ENABLE 0  // scrollsnapをオフにする
 
 #define TAP_CODE_DELAY 5
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define CLICK_LAYER_NUM 4
-#define CLICK_LAYER_LED_COLOR_HSV 132, 102, 50
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+#define KEYBALL_CPI_DEFAULT 700       // 光学センサーPMW3360DM の解像度 (CPI) の規定値
+#define KEYBALL_SCROLL_DIV_DEFAULT 5  // スクロール速度の規定値
+
+// ファームウェアのサイズを削減
+#define NO_ACTION_ONESHOT  // 392バイト
+
+// #define IGNORE_MOD_TAP_INTERRUPT  // 10バイト
+// #define TAPPING_FORCE_HOLD  // 34バイト
+// 下記三つで444バイト
+#define TAPPING_TERM 175
+// #define PERMISSIVE_HOLD
+// #define RETRO_TAPPING
