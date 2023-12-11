b0dd9d985c4fc3a2a8056d1c0f62f3058b3678e4
katotak <takuyakato.1027@gmail.com>
2023-12-11 10:55:10 +0900

    modify keymap

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,                              KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      ,
     KC_A      , KC_S      , KC_D      , KC_F      , KC_G      ,                              KC_H      , KC_J      , KC_K      , KC_L      , LT(5,KC_MINS)   ,
     KC_Z , SFT_T(KC_X) , CTL_T(KC_C) , ALT_T(KC_V), KC_B      ,                              KC_N, ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), KC_SLSH ,
-    LALT_T(KC_ESC), KC_LGUI, KC_LCTL, KC_TAB    , LT(1,KC_SPC), KC_LSFT,            KC_BSPC, LT(2,KC_ENT) , XXXXXXX , XXXXXXX  , XXXXXXX   , A(KC_GRV)
+    LALT_T(KC_ESC), KC_LCTL, KC_LGUI, KC_TAB    , LT(1,KC_SPC), KC_LSFT,            KC_BSPC, LT(2,KC_ENT) , XXXXXXX , XXXXXXX  , XXXXXXX   , A(KC_GRV)
   ),
 
   LAYOUT_universal(
