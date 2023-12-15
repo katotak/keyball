027cf660ce8c750a9cf9eb308fff8825de7578be
katotak <takuyakato.1027@gmail.com>
2023-12-16 05:06:54 +0900

    enable combo for mouse buttn

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/combo.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/combo.h
@@ -8,51 +8,18 @@
  * config.h: #define COMBO_COUNT 2（=実際に設定するコンボ数）
  */
 
-enum combos {
-  COMBO_F14,
-  COMBO_F15
-};
-
-// const uint16_t PROGMEM f14_combo[] = {KC_DOWN, KC_I, COMBO_END};
-// const uint16_t PROGMEM f15_combo[] = {G(KC_C), KC_UP, COMBO_END};
-// const uint16_t PROGMEM combo_layer_1[] = {KC_F, KC_D, COMBO_END};
-// const uint16_t PROGMEM combo_layer_2[] = {KC_J, KC_K, COMBO_END};
-// const uint16_t PROGMEM combo_layer_3[] = {KC_D, KC_S, COMBO_END};
-// const uint16_t PROGMEM combo_layer_3[] = {KC_S, KC_A, COMBO_END};
-
-const uint16_t PROGMEM combo_BRC[] = {KC_LBRC, KC_RBRC, COMBO_END};
-const uint16_t PROGMEM combo_select_BRC[] = {KC_LBRC, S(KC_EQUAL), COMBO_END};
-
-const uint16_t PROGMEM combo_S9_S0[] = {S(KC_9), S(KC_0), COMBO_END};
-const uint16_t PROGMEM combo_select_S9_S0[] = {S(KC_9), KC_MINUS, COMBO_END};
-
-const uint16_t PROGMEM combo_S_BRC[] = {S(KC_LBRC), S(KC_RBRC), COMBO_END};
-const uint16_t PROGMEM combo_select_S_BRC[] = {S(KC_LBRC), KC_RBRC, COMBO_END};
-
-const uint16_t PROGMEM combo_sumitsuki_BRC[] = {SFT_T(KC_GRAVE), KC_LBRC, COMBO_END};
-const uint16_t PROGMEM combo_select_sumitsuki_BRC[] = {SFT_T(KC_GRAVE), S(KC_0), COMBO_END};
-
-const uint16_t PROGMEM combo_MINUS_SPACE[] = {KC_RBRC, S(KC_EQUAL), COMBO_END};
+const uint16_t PROGMEM combo_btn1[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_btn2[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_btn3[] = {KC_J, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_btn4[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM combo_btn5[] = {KC_COMM, KC_DOT, COMBO_END};
 
 combo_t key_combos[] = {
-    // [COMBO_F14] = COMBO(f14_combo, KC_F14),
-    // [COMBO_F15] = COMBO(f15_combo, KC_F15)
-    // COMBO(combo_layer_1, MO(3)),
-    // COMBO(combo_layer_2, MO(3)),
-    // COMBO(combo_layer_3, MO(2)),
 
-    COMBO(combo_BRC, COMBO_BRC),
-    COMBO(combo_select_BRC, COMBO_select_BRC),
-
-    COMBO(combo_S9_S0, COMBO_S9_S0),
-    COMBO(combo_select_S9_S0, COMBO_select_S9_S0),
-
-    COMBO(combo_S_BRC, COMBO_S_BRC),
-    COMBO(combo_select_S_BRC, COMBO_select_S_BRC),
-
-    COMBO(combo_sumitsuki_BRC, COMBO_sumitsuki_BRC),
-    COMBO(combo_select_sumitsuki_BRC, COMBO_select_sumitsuki_BRC),
-
-    COMBO(combo_MINUS_SPACE, COMBO_MINUS_SPACE),
+    COMBO(combo_btn1, KC_BTN1),
+    COMBO(combo_btn2, KC_BTN2),
+    COMBO(combo_btn3, KC_BTN3),
+    COMBO(combo_btn4, KC_BTN4),
+    COMBO(combo_btn5, KC_BTN5),
 
 };
