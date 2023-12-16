307b04fa5d8300a7a2f014477bf4006324ae53f6
katotak <takuyakato.1027@gmail.com>
2023-12-16 10:08:01 +0900

    add JIS layout layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/keymap.c
@@ -59,6 +59,17 @@
 // #include "features/overrides.h"
 #include "features/combo.h"
 
+enum layer {
+  US_DEF,
+  US_SYM,
+  US_NUM,
+  JP_DEF,
+  JP_SYM,
+  JP_NUM,
+  FUNC,
+  SCRL
+};
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     
   uint8_t layer = get_highest_layer(state);
@@ -89,8 +100,38 @@ void oledkit_render_info_user(void) {
   keyball_oled_render_ballinfo();  // トラックボール情報を表示
 
   // <Layer>を表示する
-  oled_write_P(PSTR("Layer:"), false);
-  oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
+  const char *n;
+  switch (get_highest_layer(layer_state | default_layer_state)) {
+      case US_DEF:
+          n = PSTR("US_DEF");
+          break;
+      case US_SYM:
+          n = PSTR("US_SYM");
+          break;
+      case US_NUM:
+          n = PSTR("US_NUM");
+          break;
+      case JP_DEF:
+          n = PSTR("JP_DEF");
+          break;
+      case JP_SYM:
+          n = PSTR("JP_SYM");
+          break;
+      case JP_NUM:
+          n = PSTR("JP_NUM");
+          break;
+      case FUNC:
+          n = PSTR("FUNC");
+          break;
+      case SCRL:
+          n = PSTR("SCRL");
+          break;
+      default:
+          n = PSTR("Undefined");
+          break;
+    }
+    oled_write_P(PSTR("Layer: "), false);
+    oled_write_ln_P(n, false);
 
   // <マウス移動量 / クリックレイヤーしきい値>を表示
   // oled_write_P(PSTR(" MV:"), false);
@@ -103,46 +144,60 @@ void oledkit_render_info_user(void) {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  
-  [0] = LAYOUT_universal(
+
+  [US_DEF] = LAYOUT_universal(
     KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,                              KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      ,
-    KC_A      , KC_S      , KC_D      , KC_F      , KC_G      ,                              KC_H      , KC_J      , KC_K      , KC_L      , LT(5,KC_MINS)   ,
+    KC_A      , KC_S      , KC_D      , KC_F      , KC_G      ,                              KC_H      , KC_J      , KC_K      , KC_L      , LT(SCRL,KC_MINS)   ,
     KC_Z , SFT_T(KC_X) , CTL_T(KC_C) , ALT_T(KC_V), KC_B      ,                              KC_N, ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), KC_SLSH ,
-    LALT_T(KC_ESC), KC_LCTL, KC_LGUI, KC_TAB    , LT(1,KC_SPC), KC_LSFT,            KC_BSPC, LT(2,KC_ENT) , XXXXXXX , XXXXXXX  , XXXXXXX   , A(KC_GRV)
+    LALT_T(KC_ESC), KC_LCTL, KC_LGUI, KC_TAB    , LT(US_SYM,KC_SPC), KC_LSFT,            KC_BSPC, LT(US_NUM,KC_ENT) , XXXXXXX , XXXXXXX  , XXXXXXX   , A(KC_GRV)
   ),
 
-  [1] = LAYOUT_universal(
+  [US_SYM] = LAYOUT_universal(
     S(KC_1)   , S(KC_2)   , S(KC_3)   , S(KC_4)   , S(KC_5)   ,                              S(KC_6)   , S(KC_7)   , S(KC_8)   , KC_GRV    , S(KC_GRV) ,
     S(KC_SLSH), S(KC_LBRC), KC_LBRC   , S(KC_9)   , S(KC_COMM),                              KC_LNG1   , KC_EQL    , S(KC_SCLN), KC_QUOT   , S(KC_MINS),
     S(KC_BSLS), S(KC_RBRC), KC_RBRC   , S(KC_0)   , S(KC_DOT) ,                              KC_LNG2   , S(KC_EQL) , KC_SCLN   , S(KC_QUOT), KC_BSLS   ,
-    _______  , _______  , _______  , _______  , LT(3,_______) , _______   ,      _______   , LT(3,_______), _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , LT(FUNC,_______) , _______   ,      _______   , LT(FUNC,_______), _______  , _______  , _______  , _______
   ),
 
-  [2] = LAYOUT_universal(
+  [US_NUM] = LAYOUT_universal(
     KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                              KC_6      , KC_7      , KC_8      , KC_9      , KC_0      ,
-    KC_HOME   , KC_PGUP   , KC_UP     , KC_PGDN   , KC_DEL    ,                              KC_BTN4   , KC_BTN1   , KC_BTN3   , KC_BTN2   , LT(5,KC_BTN5)   ,
-    KC_END    , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_INS    ,                              XXXXXXX  ,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
-    _______  , _______  , _______  , _______  , LT(3,_______) , _______   ,      _______   , LT(3,_______), _______  , _______  , _______  , _______
+    KC_HOME   , KC_PGUP   , KC_UP     , KC_PGDN   , KC_DEL    ,                              KC_BTN4   , KC_BTN1   , KC_BTN3   , KC_BTN2   , KC_BTN5   ,
+    KC_END    , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_INS    ,                              DF(JP_DEF)  ,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
+    _______  , _______  , _______  , _______  , LT(FUNC,_______) , _______   ,      _______   , LT(FUNC,_______), _______  , _______  , _______  , _______
   ),
 
-  [3] = LAYOUT_universal(
+  [JP_DEF] = LAYOUT_universal(
+    KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,                              KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      ,
+    KC_A      , KC_S      , KC_D      , KC_F      , KC_G      ,                              KC_H      , KC_J      , KC_K      , KC_L      , LT(SCRL,KC_MINS)   ,
+    KC_Z , SFT_T(KC_X) , CTL_T(KC_C) , ALT_T(KC_V), KC_B      ,                              KC_N, ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), KC_SLSH ,
+    LALT_T(KC_ESC), KC_LCTL, KC_LGUI, KC_TAB    , LT(JP_SYM,KC_SPC), KC_LSFT,            KC_BSPC, LT(JP_NUM,KC_ENT) , XXXXXXX , XXXXXXX  , XXXXXXX   , KC_GRV
+  ),
+
+  [JP_SYM] = LAYOUT_universal(
+    S(KC_1)   , KC_LBRC   , S(KC_3)   , S(KC_4)   , S(KC_5)   ,                              KC_EQL  , S(KC_6)   , S(KC_QUOT)   , S(KC_LBRC)    , S(KC_EQL) ,
+    S(KC_SLSH), S(KC_RBRC), KC_RBRC   , S(KC_8)   , S(KC_COMM),                              KC_LNG1   , S(KC_MINS)    , KC_QUOT, S(KC_7)   , S(KC_INT1),
+    S(KC_INT3) , S(KC_BSLS), KC_BSLS   , S(KC_9)   , S(KC_DOT) ,                             KC_LNG2   , S(KC_SCLN) , KC_SCLN   , S(KC_2), KC_INT1   ,
+    _______  , _______  , _______  , _______  , LT(FUNC,_______) , _______   ,      _______   , LT(FUNC,_______), _______  , _______  , _______  , _______
+  ),
+
+  [JP_NUM] = LAYOUT_universal(
+    KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                              KC_6      , KC_7      , KC_8      , KC_9      , KC_0      ,
+    KC_HOME   , KC_PGUP   , KC_UP     , KC_PGDN   , KC_DEL    ,                              KC_BTN4   , KC_BTN1   , KC_BTN3   , KC_BTN2   , KC_BTN5   ,
+    KC_END    , KC_LEFT   , KC_DOWN   , KC_RGHT   , KC_INS    ,                              DF(US_DEF)  ,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,
+    _______  , _______  , _______  , _______  , LT(FUNC,_______) , _______   ,      _______   , LT(XXXXXXX,_______), _______  , _______  , _______  , _______
+  ),
+
+  [FUNC] = LAYOUT_universal(
     KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     ,                              KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    ,
     KC_F11    , KC_F12    , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              KBC_SAVE  , CPI_I100  , CPI_I1K   , SCRL_DVI  , XXXXXXX   , 
     XXXXXXX,ALT_T(XXXXXXX),CTL_T(XXXXXXX),SFT_T(XXXXXXX),XXXXXXX,                            KBC_RST   , CPI_D100  , CPI_D1K   , SCRL_DVD  ,XXXXXXX,
-    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
+    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , XXXXXXX
   ),
 
-  [4] = LAYOUT_universal(
+  [SCRL] = LAYOUT_universal(
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
     XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
-    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
-  ),
-
-  [5] = LAYOUT_universal(
-    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
-    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
-    XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,                              XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
-    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , _______
+    _______   , _______   , _______   , _______   , _______   , _______   ,      _______   , _______   , _______   , _______   , _______   , XXXXXXX
   )
 };
