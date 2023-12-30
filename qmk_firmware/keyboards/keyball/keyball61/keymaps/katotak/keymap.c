fcc78fb8d45539624ed736a4476ff07c7545ac10
katotak <takuyakato.1027@gmail.com>
2023-12-31 00:32:43 +0900

    keyball61 キーマップを変更、JISレイヤーを追加

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/keymap.c
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layer {
   US_DEF,
   JP_DEF,
-  FUNC,
-  SCRL
+  JP_SFT,
+  FUNC
 };
 
 // clang-format off
@@ -33,34 +33,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [US_DEF] = LAYOUT_universal(
     KC_GRV   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_EQL   ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(SCRL,KC_MINS)  , KC_SCLN  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(FUNC,KC_MINS)  , KC_SCLN  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,             KC_LBRC , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
-    KC_ESC   , XXXXXXX  , KC_LGUI  , KC_LALT  , KC_TAB   , SFT_T(KC_SPC), LT(FUNC,KC_DEL),   LT(FUNC,KC_BSPC), SFT_T(KC_ENT) , XXXXXXX , XXXXXXX , XXXXXXX , KC_LNG1 , KC_LNG2
+    KC_ESC   , A(KC_GRV), KC_LGUI  , KC_LALT  , KC_TAB   , SFT_T(KC_SPC), LT(FUNC,KC_DEL),   LT(FUNC,KC_BSPC), SFT_T(KC_ENT) , XXXXXXX , XXXXXXX , XXXXXXX , KC_LNG1 , KC_LNG2
   ),
 
   [JP_DEF] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , XXXXXXX  ,            XXXXXXX  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , LT(JP_SFT,KC_SPC), _______,      _______, LT(JP_SFT,KC_ENT), XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  , _______
   ),
 
-  [FUNC] = LAYOUT_universal(
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_HOME  , KC_PGUP  , KC_UP    , KC_PGDN  , _______  , KC_F12   ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_END   , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_INS   , _______  ,
-    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,            XXXXXXX ,  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , DF(JP_DEF), DF(US_DEF)
-  ),
-
-  [SCRL] = LAYOUT_universal(
+  [JP_SFT] = LAYOUT_universal(
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,            XXXXXXX ,  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX
+  ),
+
+  [FUNC] = LAYOUT_universal(
+    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_F12   ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  KC_HOME  , KC_PGUP  , KC_UP    , KC_PGDN  , XXXXXXX  , XXXXXXX  ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,            XXXXXXX ,  KC_END   , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_INS   , XXXXXXX  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , DF(JP_DEF), DF(US_DEF)
   )
+
 };
 // clang-format on
 
@@ -99,11 +100,11 @@ void oledkit_render_info_user(void) {
       case JP_DEF:
           n = PSTR("JP_DEF");
           break;
+      case JP_SFT:
+          n = PSTR("JP_SFT");
+          break;
       case FUNC:
           n = PSTR("FUNC");
-          break;
-      case SCRL:
-          n = PSTR("SCRL");
           break;
       default:
           n = PSTR("Undefined");
