55a1ad2fc2d5ee400b694d56e2a0bdabb87138d6
katotak <takuyakato.1027@gmail.com>
2024-01-01 02:14:31 +0900

    keyball61 キーマップを変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/keymap.c
@@ -19,47 +19,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
+#include "keymap_japanese.h"
+
 #include "features/combo.h"
 
 enum layer {
-  US_DEF,
-  JP_DEF,
-  JP_SFT,
-  FUNC
+  _US_DEF,
+  _JP_DEF,
+  _JP_SFT,
+  _FUNC
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [US_DEF] = LAYOUT_universal(
+  [_US_DEF] = LAYOUT_universal(
     KC_GRV   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_EQL   ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(FUNC,KC_MINS), KC_SCLN  ,
-    KC_LSFT  , KC_Z     , SFT_T(KC_X), CTL_T(KC_C), ALT_T(KC_V), KC_B, KC_RBRC,              KC_LBRC , KC_N     , ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), KC_SLSH, KC_BSLS  ,
-    KC_ESC   , A(KC_GRV), KC_LGUI  , KC_LALT  , KC_TAB   , SFT_T(KC_SPC), LT(FUNC,KC_DEL),   LT(FUNC,KC_BSPC), SFT_T(KC_ENT) , XXXXXXX , XXXXXXX , XXXXXXX , KC_LNG1 , KC_LNG2
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , LT(_FUNC,KC_MINS), KC_SCLN  ,
+    KC_LSFT  , KC_Z     , SFT_T(KC_X), CTL_T(KC_C), ALT_T(KC_V), KC_B, KC_LBRC,              KC_RBRC , KC_N     , ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), KC_SLSH, KC_BSLS  ,
+    KC_ESC   , A(KC_GRV), KC_LGUI  , KC_LALT  , KC_TAB   , SFT_T(KC_SPC), LT(_FUNC,KC_DEL),   LT(_FUNC,KC_BSPC), SFT_T(KC_ENT) , XXXXXXX , XXXXXXX , XXXXXXX , KC_LNG1 , KC_LNG2
   ),
 
-  [JP_DEF] = LAYOUT_universal(
-    S(KC_LBRC)  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , S(KC_MINS)  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , S(KC_7)  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , KC_BSLS  ,            KC_RBRC  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , KC_GRV  , _______  , _______  , _______  , LT(JP_SFT,KC_SPC), _______,      _______, LT(JP_SFT,KC_ENT), XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  , _______
+  [_JP_DEF] = LAYOUT_universal(
+    JP_GRV   , JP_1     , JP_2     , JP_3     , JP_4     , JP_5     ,                                  JP_6     , JP_7     , JP_8     , JP_9     , JP_0     , JP_EQL   ,
+    _______  , JP_Q     , JP_W     , JP_E     , JP_R     , JP_T     ,                                  JP_Y     , JP_U     , JP_I     , JP_O     , JP_P     , JP_QUOT  ,
+    _______  , JP_A     , JP_S     , JP_D     , JP_F     , JP_G     ,                                  JP_H     , JP_J     , JP_K     , JP_L     , LT(_FUNC,JP_MINS), JP_SCLN  ,
+    _______  , JP_Z     , SFT_T(JP_X), CTL_T(JP_C), ALT_T(JP_V), JP_B, JP_LBRC,              JP_RBRC , JP_N     , ALT_T(JP_M), CTL_T(JP_COMM), SFT_T(JP_DOT), JP_SLSH, JP_BSLS  ,
+    _______  , JP_ZKHK  , _______  , _______  , _______  , LT(_JP_SFT,KC_SPC), _______,      _______, LT(_JP_SFT,KC_ENT), XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  , _______
   ),
 
-  [JP_SFT] = LAYOUT_universal(
-    S(KC_EQL)  , S(KC_1)  , KC_LBRC  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  KC_EQL  , S(KC_6)  , S(KC_QUOT)  , S(KC_8)  , S(KC_9)  , S(KC_SCLN),
-    _______  , S(KC_Q)  , S(KC_W)  , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)  , S(KC_2),
-    _______  , S(KC_A)  , S(KC_S)  , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , S(KC_INT1), KC_QUOT,
-    _______  , S(KC_Z)  , S(KC_X)  , S(KC_C)  , S(KC_V)  , S(KC_B)  , S(KC_BSLS),             S(KC_RBRC), S(KC_N)  , S(KC_M)  , S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_INT3),
+  [_JP_SFT] = LAYOUT_universal(
+    JP_TILD  , JP_EXLM  , JP_AT    , JP_HASH  , JP_DLR   , JP_PERC  ,                                  JP_CIRC  , JP_AMPR  , JP_ASTR  , JP_LPRN  , JP_RPRN  , JP_PLUS  ,
+    _______  , S(JP_Q)  , S(JP_W)  , S(JP_E)  , S(JP_R)  , S(JP_T)  ,                                  S(JP_Y)  , S(JP_U)  , S(JP_I)  , S(JP_O)  , S(JP_P)  , S(JP_2)  ,
+    _______  , S(JP_A)  , S(JP_S)  , S(JP_D)  , S(JP_F)  , S(JP_G)  ,                                  S(JP_H)  , S(JP_J)  , S(JP_K)  , S(JP_L)  , JP_UNDS  , JP_COLN  ,
+    _______  , S(JP_Z)  , S(JP_X)  , S(JP_C)  , S(JP_V)  , S(JP_B)  , JP_LCBR  ,            JP_RCBR  , S(JP_N)  , S(JP_M)  , JP_LABK  , JP_RABK  , JP_QUES  , JP_PIPE  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  , _______
   ),
 
-  [FUNC] = LAYOUT_universal(
+  [_FUNC] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_F12   ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                  KC_HOME  , KC_PGUP  , KC_UP    , KC_PGDN  , XXXXXXX  , XXXXXXX  ,
-    _______  , XXXXXXX  , SFT_T(XXXXXXX)  , CTL_T(XXXXXXX)  , ALT_T(XXXXXXX)  , XXXXXXX  , XXXXXXX  ,            XXXXXXX ,  KC_END   , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_INS   , XXXXXXX  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , DF(JP_DEF), DF(US_DEF)
+    _______  , XXXXXXX  , SFT_T(XXXXXXX), CTL_T(XXXXXXX), ALT_T(XXXXXXX), XXXXXXX, XXXXXXX, XXXXXXX  , KC_END   , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_INS   , XXXXXXX  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , DF(_JP_DEF), DF(_US_DEF)
   )
 
 };
@@ -94,16 +96,16 @@ void oledkit_render_info_user(void) {
   // <Layer>を表示する
   const char *n;
   switch (get_highest_layer(layer_state | default_layer_state)) {
-      case US_DEF:
+      case _US_DEF:
           n = PSTR("US_DEF");
           break;
-      case JP_DEF:
+      case _JP_DEF:
           n = PSTR("JP_DEF");
           break;
-      case JP_SFT:
+      case _JP_SFT:
           n = PSTR("JP_SFT");
           break;
-      case FUNC:
+      case _FUNC:
           n = PSTR("FUNC");
           break;
       default:
