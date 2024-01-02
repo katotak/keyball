c979f731326f1e2286bf6f48a5f46b31bb706a10
katotak <takuyakato.1027@gmail.com>
2024-01-02 21:54:51 +0900

    keyball61 リファクタリング

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/features/layer.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/features/layer.h
@@ -8,4 +8,21 @@ enum layer {
   _SCRL
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    
+  uint8_t layer = get_highest_layer(state);
+  
+  // スクロールモード有効
+  keyball_set_scroll_mode(layer == _SCRL);
+
+  // // レイヤーとLEDを連動させる
+  switch(layer){
+    default:
+      rgblight_sethsv(HSV_OFF);
+      break;
+  }
+
+  return state;
+}
+
 #endif
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/features/override.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/katotak/features/override.h
@@ -12,27 +12,27 @@
 #include "layer.h"
 
 // JIS配列の記号
-const key_override_t jp_key_override_1    = ko_make_with_layers(MOD_MASK_SHIFT, JP_1   , JP_EXLM, _JP_DEF);
-const key_override_t jp_key_override_2    = ko_make_with_layers(MOD_MASK_SHIFT, JP_2   , JP_AT  , _JP_DEF);
-const key_override_t jp_key_override_3    = ko_make_with_layers(MOD_MASK_SHIFT, JP_3   , JP_HASH, _JP_DEF);
-const key_override_t jp_key_override_4    = ko_make_with_layers(MOD_MASK_SHIFT, JP_4   , JP_DLR , _JP_DEF);
-const key_override_t jp_key_override_5    = ko_make_with_layers(MOD_MASK_SHIFT, JP_5   , JP_PERC, _JP_DEF);
-const key_override_t jp_key_override_6    = ko_make_with_layers(MOD_MASK_SHIFT, JP_6   , JP_CIRC, _JP_DEF);
-const key_override_t jp_key_override_7    = ko_make_with_layers(MOD_MASK_SHIFT, JP_7   , JP_AMPR, _JP_DEF);
-const key_override_t jp_key_override_8    = ko_make_with_layers(MOD_MASK_SHIFT, JP_8   , JP_ASTR, _JP_DEF);
-const key_override_t jp_key_override_9    = ko_make_with_layers(MOD_MASK_SHIFT, JP_9   , JP_LPRN, _JP_DEF);
-const key_override_t jp_key_override_0    = ko_make_with_layers(MOD_MASK_SHIFT, JP_0   , JP_RPRN, _JP_DEF);
-const key_override_t jp_key_override_lbrc = ko_make_with_layers(MOD_MASK_SHIFT, JP_LBRC, JP_LCBR, _JP_DEF);
-const key_override_t jp_key_override_rbrc = ko_make_with_layers(MOD_MASK_SHIFT, JP_RBRC, JP_RCBR, _JP_DEF);
-const key_override_t jp_key_override_comm = ko_make_with_layers(MOD_MASK_SHIFT, JP_COMM, JP_LABK, _JP_DEF);
-const key_override_t jp_key_override_dot  = ko_make_with_layers(MOD_MASK_SHIFT, JP_DOT , JP_RABK, _JP_DEF);
-const key_override_t jp_key_override_mins = ko_make_with_layers(MOD_MASK_SHIFT, JP_MINS, JP_UNDS, _JP_DEF);
-const key_override_t jp_key_override_eql  = ko_make_with_layers(MOD_MASK_SHIFT, JP_EQL , JP_PLUS, _JP_DEF);
-const key_override_t jp_key_override_quot = ko_make_with_layers(MOD_MASK_SHIFT, JP_QUOT, JP_DQUO, _JP_DEF);
-const key_override_t jp_key_override_scln = ko_make_with_layers(MOD_MASK_SHIFT, JP_SCLN, JP_COLN, _JP_DEF);
-const key_override_t jp_key_override_slsh = ko_make_with_layers(MOD_MASK_SHIFT, JP_SLSH, JP_QUES, _JP_DEF);
-const key_override_t jp_key_override_bsls = ko_make_with_layers(MOD_MASK_SHIFT, JP_BSLS, JP_PIPE, _JP_DEF);
-const key_override_t jp_key_override_grv  = ko_make_with_layers(MOD_MASK_SHIFT, JP_GRV , JP_TILD, _JP_DEF);
+const key_override_t jp_key_override_1    = ko_make_with_layers(MOD_MASK_SHIFT, JP_1   , JP_EXLM, 1<<_JP_DEF);
+const key_override_t jp_key_override_2    = ko_make_with_layers(MOD_MASK_SHIFT, JP_2   , JP_AT  , 1<<_JP_DEF);
+const key_override_t jp_key_override_3    = ko_make_with_layers(MOD_MASK_SHIFT, JP_3   , JP_HASH, 1<<_JP_DEF);
+const key_override_t jp_key_override_4    = ko_make_with_layers(MOD_MASK_SHIFT, JP_4   , JP_DLR , 1<<_JP_DEF);
+const key_override_t jp_key_override_5    = ko_make_with_layers(MOD_MASK_SHIFT, JP_5   , JP_PERC, 1<<_JP_DEF);
+const key_override_t jp_key_override_6    = ko_make_with_layers(MOD_MASK_SHIFT, JP_6   , JP_CIRC, 1<<_JP_DEF);
+const key_override_t jp_key_override_7    = ko_make_with_layers(MOD_MASK_SHIFT, JP_7   , JP_AMPR, 1<<_JP_DEF);
+const key_override_t jp_key_override_8    = ko_make_with_layers(MOD_MASK_SHIFT, JP_8   , JP_ASTR, 1<<_JP_DEF);
+const key_override_t jp_key_override_9    = ko_make_with_layers(MOD_MASK_SHIFT, JP_9   , JP_LPRN, 1<<_JP_DEF);
+const key_override_t jp_key_override_0    = ko_make_with_layers(MOD_MASK_SHIFT, JP_0   , JP_RPRN, 1<<_JP_DEF);
+const key_override_t jp_key_override_lbrc = ko_make_with_layers(MOD_MASK_SHIFT, JP_LBRC, JP_LCBR, 1<<_JP_DEF);
+const key_override_t jp_key_override_rbrc = ko_make_with_layers(MOD_MASK_SHIFT, JP_RBRC, JP_RCBR, 1<<_JP_DEF);
+const key_override_t jp_key_override_comm = ko_make_with_layers(MOD_MASK_SHIFT, JP_COMM, JP_LABK, 1<<_JP_DEF);
+const key_override_t jp_key_override_dot  = ko_make_with_layers(MOD_MASK_SHIFT, JP_DOT , JP_RABK, 1<<_JP_DEF);
+const key_override_t jp_key_override_mins = ko_make_with_layers(MOD_MASK_SHIFT, JP_MINS, JP_UNDS, 1<<_JP_DEF);
+const key_override_t jp_key_override_eql  = ko_make_with_layers(MOD_MASK_SHIFT, JP_EQL , JP_PLUS, 1<<_JP_DEF);
+const key_override_t jp_key_override_quot = ko_make_with_layers(MOD_MASK_SHIFT, JP_QUOT, JP_DQUO, 1<<_JP_DEF);
+const key_override_t jp_key_override_scln = ko_make_with_layers(MOD_MASK_SHIFT, JP_SCLN, JP_COLN, 1<<_JP_DEF);
+const key_override_t jp_key_override_slsh = ko_make_with_layers(MOD_MASK_SHIFT, JP_SLSH, JP_QUES, 1<<_JP_DEF);
+const key_override_t jp_key_override_bsls = ko_make_with_layers(MOD_MASK_SHIFT, JP_BSLS, JP_PIPE, 1<<_JP_DEF);
+const key_override_t jp_key_override_grv  = ko_make_with_layers(MOD_MASK_SHIFT, JP_GRV , JP_TILD, 1<<_JP_DEF);
 
 // 使用する全てのキーオーバーライドをグローバルに定義
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -56,6 +56,5 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &jp_key_override_scln ,
     &jp_key_override_slsh ,
     &jp_key_override_bsls ,
-    &jp_key_override_grv  ,
     NULL  // オーバーライドの配列は Null で終了
 };
