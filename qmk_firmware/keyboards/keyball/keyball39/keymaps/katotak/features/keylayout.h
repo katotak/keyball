755affb993351a53a3b8dca77c483eda29a648e1
katotak <takuyakato.1027@gmail.com>
2025-01-28 18:30:01 +0900

    キーマップを変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/keylayout.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/keylayout.h
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q       , KC_W       , KC_E       , KC_R       , KC_T       ,                              KC_Y       , KC_U       , KC_I       , KC_O       , KC_P       ,
     KC_A       , KC_S       , KC_D       , KC_F       , KC_G       ,                              KC_H       , KC_J       , KC_K     , KC_L, LT(_SCROLL_,KC_MINS),
     GUI_T(KC_Z), SFT_T(KC_X), CTL_T(KC_C), ALT_T(KC_V), KC_B       ,                              KC_N, ALT_T(KC_M), CTL_T(KC_COMM), SFT_T(KC_DOT), GUI_T(KC_SLSH),
-    XXXXXXX, XXXXXXX, XXXXXXX, CTL_T(KC_TAB), LT(_US_SYM_,KC_SPC), LT(_FUNC_,KC_ENT),    LT(_FUNC_,KC_BSPC), KC_RSFT, XXXXXXX, XXXXXXX   , XXXXXXX    , XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LT(_US_SYM_,KC_SPC), CTL_T(KC_ENT),    LT(_FUNC_,KC_BSPC), KC_RSFT, XXXXXXX, XXXXXXX   , XXXXXXX    , XXXXXXX
   ),
 
   [_US_SYM_] = LAYOUT_universal(
