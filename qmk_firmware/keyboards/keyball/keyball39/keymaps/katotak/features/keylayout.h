ba1a183ac39b0166856d718a6a06561014283d97
katotak <takuyakato.1027@gmail.com>
2025-08-09 10:52:05 +0900

    キーマップを変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/keylayout.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/katotak/features/keylayout.h
@@ -9,27 +9,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q       , KC_W       , KC_E       , KC_R       , KC_T       ,                              KC_Y       , KC_U       , KC_I       , KC_O       , KC_P       ,
     KC_A       , KC_S       , KC_D       , KC_F       , KC_G       ,                              KC_H       , KC_J       , KC_K     , KC_L, LT(_SCROLL_,KC_MINS),
     GUI_T(KC_Z), ALT_T(KC_X), CTL_T(KC_C), SFT_T(KC_V), KC_B       ,                              KC_N, SFT_T(KC_M), CTL_T(KC_COMM), ALT_T(KC_DOT), GUI_T(KC_SLSH),
-    XXXXXXX, XXXXXXX, XXXXXXX, LT(_FUNC_,KC_TAB), LT(_US_SYM_,KC_SPC), CTL_T(KC_ENT),    LT(_FUNC_,KC_BSPC), KC_RSFT, XXXXXXX, XXXXXXX   , XXXXXXX    , XXXXXXX
+    KC_ESC, XXXXXXX, XXXXXXX, LT(_FUNC_,KC_TAB), LT(_US_SYM_,KC_SPC), CTL_T(KC_ENT),    LT(_FUNC_,KC_BSPC), KC_RSFT, XXXXXXX, XXXXXXX   , XXXXXXX    , KC_DEL
   ),
 
   [_US_SYM_] = LAYOUT_universal(
     KC_1       , KC_2       , KC_3       , KC_4       , KC_5       ,                              KC_6       , KC_7       , KC_8       , KC_9       , KC_0       ,
-    KC_INS     , KC_HOME    , KC_UP      , KC_END     , KC_PGUP    ,                              XXXXXXX    , KC_GRV     , KC_EQL     , KC_SCLN    , KC_QUOT    ,
-    GUI_T(KC_PSCR), ALT_T(KC_LEFT), CTL_T(KC_DOWN), SFT_T(KC_RGHT), KC_PGDN,                      DF(_JP_DEF_), KC_LSFT, CTL_T(KC_LBRC), ALT_T(KC_RBRC), GUI_T(KC_BSLS),
+    KC_INS     , KC_HOME    , KC_UP      , KC_END     , KC_PGUP    ,                              KC_LANGUAGE_2, KC_GRV     , KC_EQL     , KC_SCLN    , KC_QUOT    ,
+    GUI_T(KC_PSCR), ALT_T(KC_LEFT), CTL_T(KC_DOWN), SFT_T(KC_RGHT), KC_PGDN,                      KC_LANGUAGE_1, KC_LSFT, CTL_T(KC_LBRC), ALT_T(KC_RBRC), GUI_T(KC_BSLS),
     _______    , _______    , _______    , _______    , _______    , _______    ,    _______    , _______    , _______    , _______    , _______    , _______
   ),
 
   [_JP_DEF_] = LAYOUT_universal(
-    KC_Q       , KC_W       , KC_E       , KC_R       , KC_T       ,                              KC_Y       , KC_U       , KC_I       , KC_O       , KC_P       ,
-    KC_A       , KC_S       , KC_D       , KC_F       , KC_G       ,                              KC_H       , KC_J       , KC_K     , KC_L, LT(_SCROLL_,KC_MINS),
-    GUI_T(KC_Z), ALT_T(KC_X), CTL_T(KC_C), SFT_T(KC_V), KC_B       ,                              KC_N, SFT_T(KC_M), CTL_T(KC_COMM), ALT_T(KC_DOT), GUI_T(KC_SLSH),
-    XXXXXXX, XXXXXXX, XXXXXXX, LT(_FUNC_,KC_TAB), LT(_JP_SYM_,KC_SPC), CTL_T(KC_ENT),    LT(_FUNC_,KC_BSPC), KC_RSFT, XXXXXXX, XXXXXXX   , XXXXXXX    , XXXXXXX
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
+    _______    , _______    , _______    , _______    , _______    , _______    ,    _______    , _______    , _______    , _______    , _______    , _______
   ),
 
   [_JP_SYM_] = LAYOUT_universal(
-    KC_1       , KC_2       , KC_3       , KC_4       , KC_5       ,                              KC_6       , KC_7       , KC_8       , KC_9       , KC_0       ,
-    KC_INS     , KC_HOME    , KC_UP      , KC_END     , KC_PGUP    ,                              XXXXXXX    , KC_GRV     , KC_EQL     , KC_SCLN    , KC_QUOT    ,
-    GUI_T(KC_PSCR), ALT_T(KC_LEFT), CTL_T(KC_DOWN), SFT_T(KC_RGHT), KC_PGDN,                      DF(_US_DEF_), KC_LSFT, CTL_T(KC_LBRC), ALT_T(KC_RBRC), GUI_T(KC_BSLS),
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
+    XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,                              XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    , XXXXXXX    ,
     _______    , _______    , _______    , _______    , _______    , _______    ,    _______    , _______    , _______    , _______    , _______    , _______
   ),
 
